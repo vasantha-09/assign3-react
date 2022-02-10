28b7e720e70c89dd753c06ae711102880815ca63
vasantha <vasanthalakshmiballipati@gmail.com>
2022-02-10 22:10:55 +0530

    my commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import Comment from './Comment.js'
+ function App(){
+ 
+    return(
+        <Comment/>
+    )
+    };
+    ReactDom.render(
+        <>
+        <App/>
+        </>,
+   document.getElementById('root')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+    )
 
