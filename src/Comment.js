2fe6600b4b321d78fd7377209ce9eaca00481ddc
vasantha <vasanthalakshmiballipati@gmail.com>
2022-02-10 22:10:24 +0530

    commited

---
+++ src/Comment.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Faker from 'faker';
+function Comment(){
+    return(
+<div >
+        <table className="ui very basic collapsing celled table">
+  <thead>
+    <tr><th>Employee</th>
+    <th>Correct Guesses</th>
+  </tr></thead>
+  <tbody>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={Faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Lena
+            <div className="sub header">Human Resources
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        22
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={Faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Matthew
+            <div className="sub header">Fabric Design
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        15
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={Faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Lindsay
+            <div className="sub header">Entertainment
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        12
+      </td>
+    </tr>
+    <tr>
+      <td>
+        <h4 className="ui image header">
+          <img src={Faker.image.image()} className="ui mini rounded image"/>
+          <div className="content">
+            Mark
+            <div className="sub header">Executive
+          </div>
+        </div>
+      </h4></td>
+      <td>
+        11
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
+    )
+}
+export default Comment;
